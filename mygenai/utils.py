b9074ec4b6ab3aec4bfb169566ef79738bded8cb
jphaupt <jphaupt@protonmail.com>
2025-03-24 21:25:15 +0100

    complete basic visualisation/exploratory analysis

--- mygenai/utils.py
+++ mygenai/utils.py
@@ -2,6 +2,13 @@
 import rdkit
 from rdkit.Chem import rdDepictor
 from rdkit.Chem.Draw import rdMolDraw2D
+import py3Dmol
+from rdkit.Chem import AllChem
+import rdkit.Chem as Chem
+from rdkit.Chem import QED, Crippen, rdMolDescriptors, rdmolops
+from rdkit.Geometry.rdGeometry import Point3D
+import torch
+
 def moltosvg(mol,molSize=(450,150),kekulize=True):
     # taken from
     # https://rdkit.blogspot.com/2015/02/new-drawing-code.html
@@ -21,3 +28,157 @@ def moltosvg(mol,molSize=(450,150),kekulize=True):
     # Here are some fixes to make it work in the notebook, although I think
     # the underlying issue needs to be resolved at the generation step
     return svg.replace('svg:','')
+
+# The following visualisation helper functions were taken from
+# https://colab.research.google.com/github/chaitjo/geometric-gnn-dojo/blob/main/geometric_gnn_101.ipynb#scrollTo=ExJ0b3xcQl5n
+allowable_atoms = [
+    "H",
+    "C",
+    "N",
+    "O",
+    "F",
+    "C",
+    "Cl",
+    "Br",
+    "I",
+    "H",
+    "Unknown",
+]
+
+def to_atom(t):
+    try:
+        return allowable_atoms[int(t.argmax())]
+    except:
+        return "C"
+
+
+def to_bond_index(t):
+    t_s = t.squeeze()
+    return [1, 2, 3, 4][
+        int(
+            torch.dot(
+                t_s,
+                torch.tensor(
+                    range(t_s.size()[0]), dtype=torch.float, device=t.device
+                ),
+            ).item()
+        )
+    ]
+
+def to_rdkit(data, device=None):
+    has_pos = False
+    node_list = []
+    for i in range(data.x.size()[0]):
+        node_list.append(to_atom(data.x[i][:5]))
+
+    # create empty editable mol object
+    mol = Chem.RWMol()
+    # add atoms to mol and keep track of index
+    node_to_idx = {}
+    invalid_idx = set([])
+    for i in range(len(node_list)):
+        if node_list[i] == "Stop" or node_list[i] == "H":
+            invalid_idx.add(i)
+            continue
+        a = Chem.Atom(node_list[i])
+        molIdx = mol.AddAtom(a)
+        node_to_idx[i] = molIdx
+
+    added_bonds = set([])
+    for i in range(0, data.edge_index.size()[1]):
+        ix = data.edge_index[0][i].item()
+        iy = data.edge_index[1][i].item()
+        bond = to_bond_index(data.edge_attr[i])  # <font color='red'>TODO</font> fix this
+        # bond = 1
+        # add bonds between adjacent atoms
+
+        if data.edge_attr[i].sum() == 0:
+          continue
+
+        if (
+            (str((ix, iy)) in added_bonds)
+            or (str((iy, ix)) in added_bonds)
+            or (iy in invalid_idx or ix in invalid_idx)
+        ):
+            continue
+        # add relevant bond type (there are many more of these)
+
+        if bond == 0:
+            continue
+        elif bond == 1:
+            bond_type = Chem.rdchem.BondType.SINGLE
+            mol.AddBond(node_to_idx[ix], node_to_idx[iy], bond_type)
+        elif bond == 2:
+            bond_type = Chem.rdchem.BondType.DOUBLE
+            mol.AddBond(node_to_idx[ix], node_to_idx[iy], bond_type)
+        elif bond == 3:
+            bond_type = Chem.rdchem.BondType.TRIPLE
+            mol.AddBond(node_to_idx[ix], node_to_idx[iy], bond_type)
+        elif bond == 4:
+            bond_type = Chem.rdchem.BondType.SINGLE
+            mol.AddBond(node_to_idx[ix], node_to_idx[iy], bond_type)
+
+        added_bonds.add(str((ix, iy)))
+
+    if has_pos:
+        conf = Chem.Conformer(mol.GetNumAtoms())
+        for i in range(data.pos.size(0)):
+            if i in invalid_idx:
+                continue
+            p = Point3D(
+                data.pos[i][0].item(),
+                data.pos[i][1].item(),
+                data.pos[i][2].item(),
+            )
+            conf.SetAtomPosition(node_to_idx[i], p)
+        conf.SetId(0)
+        mol.AddConformer(conf)
+
+    # Convert RWMol to Mol object
+    mol = mol.GetMol()
+    mol_frags = rdmolops.GetMolFrags(mol, asMols=True, sanitizeFrags=False)
+    largest_mol = max(mol_frags, default=mol, key=lambda m: m.GetNumAtoms())
+    return largest_mol
+
+
+def MolTo3DView(mol, size=(300, 300), style="stick", surface=False, opacity=0.5):
+    """Draw molecule in 3D
+
+    Args:
+    ----
+        mol: rdMol, molecule to show
+        size: tuple(int, int), canvas size
+        style: str, type of drawing molecule
+               style can be 'line', 'stick', 'sphere', 'carton'
+        surface, bool, display SAS
+        opacity, float, opacity of surface, range 0.0-1.0
+    Return:
+    ----
+        viewer: py3Dmol.view, a class for constructing embedded 3Dmol.js views in ipython notebooks.
+    """
+    assert style in ('line', 'stick', 'sphere', 'carton')
+
+    mol = Chem.AddHs(mol)
+    AllChem.EmbedMolecule(mol)
+    AllChem.MMFFOptimizeMolecule(mol, maxIters=200)
+    mblock = Chem.MolToMolBlock(mol)
+    viewer = py3Dmol.view(width=size[0], height=size[1])
+    viewer.addModel(mblock, 'mol')
+    viewer.setStyle({style:{}})
+    if surface:
+        viewer.addSurface(py3Dmol.SAS, {'opacity': opacity})
+    viewer.zoomTo()
+    return viewer
+
+def smi2conf(smiles):
+    '''Convert SMILES to rdkit.Mol with 3D coordinates'''
+    mol = Chem.MolFromSmiles(smiles)
+    if mol is not None:
+        mol = Chem.AddHs(mol)
+        AllChem.EmbedMolecule(mol)
+        AllChem.MMFFOptimizeMolecule(mol, maxIters=200)
+        return mol
+    else:
+        return None
+
+visualise_molecule = lambda x: MolTo3DView(smi2conf(Chem.MolToSmiles(to_rdkit(x))))
